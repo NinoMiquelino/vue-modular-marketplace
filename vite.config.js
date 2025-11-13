5e8defc9511f71dc50a2f5225c69f0edfc2f1f0a
Onivaldo Miquelino <ninomiquelino@gmail.com>
2025-11-12 22:57:49 -0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/main.scss";`
+      }
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['vue', 'vue-router', 'pinia'],
+          'graphql': ['@apollo/client', '@vue/apollo-composable'],
+          'ui': ['lucide-vue-next']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 600
+  }
+})
